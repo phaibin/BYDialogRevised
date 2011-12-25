8ac3ed24fa33c7d270e59b6d675cc004f1c0cdf8
Leon <phaibin@gmail.com>
2011-12-25 10:41:45 +0800

    Initial Commit

---
+++ BYDialogRevised/BYDialogRevisedAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  BYDialogRevisedAppDelegate.h
+//  BYDialogRevised
+//
+//  Created by  on 12/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BYDialogRevisedViewController;
+
+@interface BYDialogRevisedAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet BYDialogRevisedViewController *viewController;
+
+@end
---
+++ BYDialogRevised/BYDialogRevisedViewController.h
@@ -0,0 +1,13 @@
+//
+//  BYDialogRevisedViewController.h
+//  BYDialogRevised
+//
+//  Created by  on 12/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BYDialogRevisedViewController : UIViewController
+
+@end
